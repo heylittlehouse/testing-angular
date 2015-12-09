716c0e58d1f130111b6a215be4dbe56fa50f29bd
Michael <email": null>
2015-12-09 11:43:03 -0500

    Get basic front page loaded and linking

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var app = angular.module("reddit", []);
 app.controller("frontPage", function($scope, $http){
   $http.get('http://api.reddit.com').then(function(response){
     $scope.posts = response.data.data.children
-
+    console.log(response.data.data.children)
   });
 });
