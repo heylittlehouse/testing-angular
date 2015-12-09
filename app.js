3b8c41b391af9a016b92d58b56dcfb6d96959866
Michael <email": null>
2015-12-09 12:31:34 -0500

    Get array of json comments objects

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ var app = angular.module("reddit", []);
 app.controller("frontPage", function($scope, $http){
   $http.get('http://api.reddit.com').then(function(response){
     $scope.posts = response.data.data.children
-    console.log(response.data.data.children)
   });
+
+  $scope.comments = function(){
+    event.preventDefault();
+    $http.get(event.target.href).then(function(response){
+      var commentsArray = response.data[1].data.children
+      console.log(commentsArray)
+    })
+  }
 });
